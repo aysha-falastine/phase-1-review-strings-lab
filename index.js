6bc3dfb52ada3801fd11b1965e27adab3d22b76a
asha mohamed <falastine924@gmail.com>
2025-06-03 23:06:06 +0300

    index.html

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+// 1. Define currentUser
+const currentUser = 'Asha'; 
+
+// 2. Define welcomeMessage
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// 3. Define excitedWelcomeMessage
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// 4. Define shortGreeting
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+
